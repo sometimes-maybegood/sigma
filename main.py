2894d7e050de12f7809317f7338e614b2685825b
sometimes-maybegood <maratakhun@gmail.com>
2024-12-08 15:21:09 +0300

    1

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+import sqlite3
+from PyQt6 import QtWidgets, uic
+
+
+class CoffeeApp(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(CoffeeApp, self).__init__()
+        uic.loadUi('main.ui', self)
+        self.load_coffee_data()
+
+    def load_coffee_data(self):
+        conn = sqlite3.connect('coffee.sqlite')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM coffee")
+        rows = cursor.fetchall()
+        self.tableWidget.setRowCount(len(rows))
+        self.tableWidget.setColumnCount(7)
+        for row_index, row_data in enumerate(rows):
+            for column_index, item in enumerate(row_data):
+                self.tableWidget.setItem(row_index, column_index, QtWidgets.QTableWidgetItem(str(item)))
+
+        conn.close()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = CoffeeApp()
+    window.show()
+    sys.exit(app.exec())
